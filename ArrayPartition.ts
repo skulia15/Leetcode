8cae311b600a8a8e2b0df7469f5fd0cda1ffe426
Skúli Arnarsson <skuliarnarsson@gmail.com>
2024-11-21 12:58:41 +0000

    Implement arrayPairSum function to calculate sum of paired elements from sorted array

---
+++ ArrayPartition.ts
@@ -0,0 +1,12 @@
+function arrayPairSum(nums: number[]): number {
+  nums.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < nums.length; i += 2) {
+    sum += nums[i];
+  }
+  return sum;
+}
+
+let nums = [1, 4, 3, 2];
+
+arrayPairSum(nums);
