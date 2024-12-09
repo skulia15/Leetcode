4ba3e08575f767fa3017f3606622f7cb2172ef24
Skúli Arnarsson <skuliarnarsson@gmail.com>
2024-12-09 10:09:39 +0000

    improved Day 5

--- Advent of Code 2024/Day 5/code.ts
+++ Advent of Code 2024/Day 5/code.ts
@@ -1,53 +1,47 @@
 import { readFileSync } from "fs";
 
 const day5 = () => {
+    // TODO: Can be improved by implementing A Directed Graph with topological sort
+
     // Read input file and split on empty line
     const input = readFileSync("input.txt").toString().split("\r\n\r\n");
     const rules = input[0].split("\r\n");
-    const orders = input[1];
+    const orders = input[1].split("\r\n");
 
-    // Construct a map of rules
-    const ruleMap = new Map<string, string[]>();
+    const ruleSet = new Set();
     for (const rule of rules) {
-        const [key, value] = rule.split("|");
-        let prevValues = ruleMap.get(key);
-        ruleMap.set(key, prevValues ? [...prevValues, value] : [value]);
+        ruleSet.add(rule); // Example: "47|53"
     }
-
-    const lines = orders.split("\r\n");
-    let validLists: string[] = [];
-
-    // Loop through each line(order) of the orders
-    for (let i = 0; i < lines.length; i++) {
-        let validList = true;
-        const numbers = lines[i].split(",");
-        for (let j = 0; j < numbers.length - 1; j++) {
-            const num = numbers[j];
-            const nextNum = numbers[j + 1];
-
-            // check each value
-            const rulesForValue = ruleMap.get(num);
-            if (rulesForValue?.includes(nextNum)) {
-                // console.log("Rules for " + num + " includes " + nextNum);
-            } else {
-                console.log("rulesForValue", rulesForValue);
-                validList = false;
-                break;
-            }
-        }
-        if (validList) {
-            console.log("List IS valid");
-            validLists.push(lines[i]);
-        }
-    }
-    console.log(validLists);
 
     let sum = 0;
-    for (let i = 0; i < validLists.length; i++) {
-        const arr = validLists[i].split(",");
-        var middle = arr[Math.round((arr.length - 1) / 2)];
-        sum += parseInt(middle);
+    let validLists = [];
+
+    // Loop through each line(order) of the orders
+    for (const line of orders) {
+        const pages = line.split(",");
+
+        let validList = true;
+        for (let i = 0; i < pages.length; i++) {
+            for (let j = i + 1; j < pages.length; j++) {
+                const pairRule = `${pages[i]}|${pages[j]}`;
+
+                // If the rule exists, it's valid; otherwise, it's invalid
+                if (!ruleSet.has(pairRule)) {
+                    validList = false;
+                    break;
+                }
+            }
+            if (!validList) break;
+        }
+        if (validList) {
+            validLists.push(pages);
+            const middlePage = pages[Math.floor((pages.length - 1) / 2)];
+            sum += parseInt(middlePage, 10);
+        }
     }
+
+    console.log(validLists);
+
     console.log("part 1 answer:", sum);
 };
 
