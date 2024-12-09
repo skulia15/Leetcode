2cfa6fe11830568c51322ecc0601e24a185b1c04
Skúli Arnarsson <skuliarnarsson@gmail.com>
2024-12-09 10:32:03 +0000

    day 5 part 2

--- Advent of Code 2024/Day 5/code.ts
+++ Advent of Code 2024/Day 5/code.ts
@@ -1,5 +1,28 @@
 import { readFileSync } from "fs";
 
+const sortInvalid = (invalidList: string[], rules: Set<string>) => {
+    const sorted: string[] = [];
+    for (const page of invalidList) {
+        let inserted = false;
+
+        for (let i = 0; i < sorted.length; i++) {
+            const existingPage = sorted[i];
+            const rule = `${page}|${existingPage}`;
+
+            // If there's a rule that the current page must come before the existing page
+            if (rules.has(rule)) {
+                sorted.splice(i, 0, page);
+                inserted = true;
+                break;
+            }
+        }
+        if (!inserted) {
+            sorted.push(page);
+        }
+    }
+    return sorted;
+};
+
 const day5 = () => {
     // TODO: Can be improved by implementing A Directed Graph with topological sort
 
@@ -8,13 +31,14 @@ const day5 = () => {
     const rules = input[0].split("\r\n");
     const orders = input[1].split("\r\n");
 
-    const ruleSet = new Set();
+    const ruleSet = new Set<string>();
     for (const rule of rules) {
         ruleSet.add(rule); // Example: "47|53"
     }
 
     let sum = 0;
     let validLists = [];
+    let invalidLists = [];
 
     // Loop through each line(order) of the orders
     for (const line of orders) {
@@ -36,13 +60,22 @@ const day5 = () => {
         if (validList) {
             validLists.push(pages);
             const middlePage = pages[Math.floor((pages.length - 1) / 2)];
-            sum += parseInt(middlePage, 10);
+            sum += parseInt(middlePage);
+        } else {
+            invalidLists.push(pages);
         }
     }
 
-    console.log(validLists);
-
     console.log("part 1 answer:", sum);
+
+    let sumPt2 = 0;
+
+    for (const invalidList of invalidLists) {
+        const sorted = sortInvalid(invalidList, ruleSet);
+        const middlePage = sorted[Math.floor((sorted.length - 1) / 2)];
+        sumPt2 += parseInt(middlePage);
+    }
+    console.log("part 2 answer:", sumPt2);
 };
 
 day5();
